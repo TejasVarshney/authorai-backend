62cb28325f9adf87c8d54274abc46ef7bbe6adf2
= <=>
2025-04-21 18:21:58 +0530

    .

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import json
 import fastapi 
-import os
 from file_handling import FileHandler
 from bookGeneration import generate as bookGenerate
 from fastapi.middleware.cors import CORSMiddleware
@@ -10,12 +9,11 @@ app = fastapi.FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Update this with your Netlify URL in production
+    allow_origins=["http://localhost:5173"],  # Your React app's URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 @app.post("/")
 def call(topic: str = fastapi.Body(...)) :
     print("Started Generating Book for : " + topic)
@@ -27,7 +25,6 @@ def call(topic: str = fastapi.Body(...)) :
     print("Completed Generating Book for : " + topic)
     return res
 
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+# if __name__ == "__main__" :
+#     import uvicorn
+#     uvicorn.run(app)
